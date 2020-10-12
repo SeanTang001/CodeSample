7c15d20e87e1c8606224ecb9b2ed694af851c977
Xiyuan0516 <seantang001@hotmail.com>
2020-10-12 15:14:06 -0700

    cleaned up run.py output

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os, subprocess, signal, time
 
 print("[+] started api server, web server, scraper, database loader, graph loader, download loader") 
-a = subprocess.Popen("cd ./DataBase/inventory; python cron.py", shell=True)
-b = subprocess.Popen("python ./FlaskApp/server.py", shell=True)
-c = subprocess.Popen("cd DataBase; uvicorn api:app", shell=True)
+a = subprocess.Popen("cd ./DataBase/inventory; python cron.py", shell=True, stdout=subprocess.PIPE)
+b = subprocess.Popen("python ./FlaskApp/server.py", shell=True, stdout=subprocess.PIPE)
+c = subprocess.Popen("cd DataBase; uvicorn api:app", shell=True, stdout=subprocess.PIPE)
 
 print "\n"
 while True:
