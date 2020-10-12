492f4d1aacc769984a245634ff100cf85af97497
Xiyuan0516 <seantang001@hotmail.com>
2020-10-11 23:56:22 -0700

    added starter script

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os, subprocess, signal, time
 
 print("[+] started api server, web server, scraper, database loader, graph loader, download loader") 
-a = subprocess.Popen("cd ./DataBase/inventory; python cron.py > /dev/null", shell=True)
-b = subprocess.Popen("python ./FlaskApp/server.py > /dev/null", shell=True)
-c = subprocess.Popen("cd DataBase; uvicorn api:app > /dev/null", shell=True)
+a = subprocess.Popen("cd ./DataBase/inventory; python cron.py", shell=True)
+b = subprocess.Popen("python ./FlaskApp/server.py", shell=True)
+c = subprocess.Popen("cd DataBase; uvicorn api:app", shell=True)
 
 print "\n"
 while True:
